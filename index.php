a9643c9920069111acb5cf0fc152853ac4049d19
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 14:08:27 +0200

    evo

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
+<?php
+    $name="coucou";
+?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
         <title>PHP ex1</title>
     </head>
     <body >
-        <?php
-        $name="coucou";
-        echo ("PHP Wellcome ");
-        echo ($name);
-        ?>
+        <p>
+            <?php
+            echo ('PHP Wellcome '.$name);
+            ?>
+        </p>
     </body>
 </html>
