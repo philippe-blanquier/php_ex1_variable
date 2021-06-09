7c6a1fde454f2a9ddaca56f5428a9872713885cd
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 10:14:59 +0200

    creation

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex1</title>
+    </head>
+    <body >
+        <?php
+        $name="coucou";
+        echo ("PHP Wellcome ");
+        echo ($name);
+        ?>
+    </body>
+</html>
